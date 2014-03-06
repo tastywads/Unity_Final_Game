e5144cc1491c4d07213b5096f0a8e1683e5295d5
wysley <wysley@gmail.com>
2014-03-05 21:13:17 -0800

    menu networking and comments on existing code
    finished basic menu and networking for hosting/connecting
    added comments to all existing code

--- Assets/Scripts/MenuButtonManager.cs
+++ Assets/Scripts/MenuButtonManager.cs
@@ -28,55 +28,90 @@ public class MenuButtonManager : MonoBehaviour
 
 	void OnGUI()
 	{
+		//The game is on menu screen
 		if(onMenu)
 		{
+			//Not on list of games screen
 			if(connectGame == false)
 			{
+				//Host Game button
 				if(GUI.Button(new Rect( (Screen.width/5)-75, (Screen.height/2)-50, 150, 100), "Host Game"))
 				{
 					hostGame = true;
 				}
-				
+
+				//Go to the list of games screen
 				if(GUI.Button(new Rect( (Screen.width/2)-75, (Screen.height/5)-50, 150, 100), "Connect to Game"))
 				{
+					//Get all the hosted games
 					networkScript.RefreshHostList(gameTypeName);
+					//Gets rid of "Enter Game Name" box
 					hostGame = false;
+					//Head to list of games screen
 					connectGame = true;
 				}
 
+				//Host Game button is pushed
 				if(hostGame == true)
 				{
+					//"Enter Game Name" box
 					GUI.Box(new Rect( (Screen.width/2)-125, ((Screen.height/4)*3)-50, 250, 100), "Please enter game name:");
 
+					//Take an event that just happened (in this case, we're looking for keyboard click)
 					Event e = Event.current;
+					//Checks if the event is "Enter" key
 					if(e.keyCode == KeyCode.Return && returnButtonPressed == false)
 					{
-						Debug.Log("Return");
 						returnButtonPressed = true;
+						//Starts and hosts game
 						networkScript.StartServer(gameTypeName, gameName);
-						MenuCamera.depth=0;
+						//Changes camera
+						MenuCamera.depth=-1;
+						//No longer in menu
 						onMenu = false;
 					}
 					else
 					{
+						//Enter Name textbox
 						gameName = GUI.TextField(new Rect( (Screen.width/2)-100, ((Screen.height/4)*3)-10, 200, 20), gameName, 25);
 					}
 				}
 			}
+			//On list of games screen
 			else
 			{
+				//If there are games
 				if(hostData != null)
 				{
+					//Print them in a list
 					for(int i=0; i<hostData.Length; ++i)
 					{
+						//If respective game button is clicked
 						if(GUI.Button(new Rect((Screen.width/3)-150, 65f + (30f * i), 300f, 30f), hostData[i].gameName))
 						{
+							//Connects to game
 							Network.Connect(hostData[i]);
 							Debug.Log("Connecting...");
-							MenuCamera.depth=0;
+							//Changes camera
+							MenuCamera.depth=-1;
+							//No longer on menu screens
 							onMenu = false;
 						}
 					}
+				}
+
+				//Back button
+				if(GUI.Button(new Rect( ((Screen.width/4)*3)-75, ((Screen.height/3)*2)-30, 150f, 60f), "Back"))
+				{
+					//No longer of list of games screen
+					connectGame = false;
+				}
+
+				//Refresh button
+				if(GUI.Button(new Rect( ((Screen.width/4)*3)-75, (Screen.height/3)-30, 150f, 60f), "Refresh"))
+				{
+					//Get all the hosted games
+					networkScript.RefreshHostList(gameTypeName);
 				}
 			}
 		}
@@ -86,9 +121,12 @@ public class MenuButtonManager : MonoBehaviour
 	{
 		switch(msEvent)
 		{
+		//If a list of games is recieved
 		case MasterServerEvent.HostListReceived:
 			{
+				//Localize list of games
 				hostData = MasterServer.PollHostList();
+				/*
 				if(hostData == null || hostData.Length == 0)
 				{
 					Debug.Log ("No active servers have been found.");
@@ -97,13 +135,18 @@ public class MenuButtonManager : MonoBehaviour
 				{
 					Debug.Log(hostData.Length + " server(s) found.");
 				}
+				*/
 			}
 			break;
 		}
 	}
 
+	//Public function to send you back to menu
 	public void OnMenu()
 	{
+		//On menu screen
 		onMenu = true;
+		//Changes back to menu camera
+		MenuCamera.depth=1;
 	}
 }
--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class NetworkManager : MonoBehaviour 
 {
+	//Start server
 	public void StartServer(string gameTypeName, string gameName)
 	{
 		Debug.Log("Starting a new server...");
 		Network.InitializeServer (2, 25002, false);
 		MasterServer.RegisterHost(gameTypeName, gameName);
 	}
-	
+
+	//Refresh server list
 	public void RefreshHostList(string gameTypeName)
 	{
 		Debug.Log("Refreshing...");
