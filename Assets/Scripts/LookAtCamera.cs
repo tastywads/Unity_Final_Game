5b7745211d94f714a95f04c1228fb535690d48af
Brad <brad.miller.dev@gmail.com>
2014-03-05 21:27:51 -0800

    Added Camera and player movement

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour {
+
+	public GameObject target;
+
+	void LateUpdate() 
+	{
+		transform.LookAt (target.transform);
+	}
+}
---
+++ Assets/Scripts/ThirdPersonController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonController : MonoBehaviour 
+{
+	public float moveSpeed;
+	public float turnSpeed;
+
+	private Transform myTransform;
+	
+	void Start ()
+	{
+		myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float horizontal = Input.GetAxis ("Horizontal") * turnSpeed * Time.deltaTime;
+		myTransform.Rotate (0, horizontal, 0);
+
+		float vertical = Input.GetAxis ("Vertical") * moveSpeed * Time.deltaTime;
+		myTransform.Translate (0, 0, vertical);
+	
+	}
+}
