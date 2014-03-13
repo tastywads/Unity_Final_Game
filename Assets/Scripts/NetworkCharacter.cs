d0b5c1683fdf9cca9a7edb206525aea7160607bc
wysley <wysley@gmail.com>
2014-03-12 20:40:54 -0700

    removed debug messages in NetworkCharacter

--- Assets/Scripts/NetworkCharacter.cs
+++ Assets/Scripts/NetworkCharacter.cs
@@ -18,14 +18,9 @@ public class NetworkCharacter : Photon.MonoBehaviour
 
 	void Update()
 	{
-		if(myPhotonView.isMine)
-		{
-			Debug.Log ("MINE");
-		}
-
 		if(!(myPhotonView.isMine))
 		{
-			Debug.Log("NOT MINE");
+			//Smoothes out visuals
 			myTransform.position = Vector3.Lerp (myTransform.position, realPosition, 0.1f);
 			myTransform.rotation = Quaternion.Lerp (myTransform.rotation, realRotation, 0.1f);
 		}
@@ -33,18 +28,15 @@ public class NetworkCharacter : Photon.MonoBehaviour
 
 	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
 	{
-		Debug.Log ("IN SERIALIZE");
 		if(stream.isWriting)
 		{
 			//This is our player. We need to send our actual position to the network
-			Debug.Log ("WRITING");
 			stream.SendNext(myTransform.position);
 			stream.SendNext(myTransform.rotation);
 		}
 		else
 		{
 			//This is someone else's player. We need to receive their position
-			Debug.Log ("SENDING");
 			realPosition = (Vector3)stream.ReceiveNext();
 			realRotation = (Quaternion)stream.ReceiveNext();
 		}
